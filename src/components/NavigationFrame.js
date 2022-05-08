f4060f782fb0b1710ee1da68cbd7736855ca731e
Harsh Singh <sujeetsingh734321@gmail.com>
2022-05-08 16:44:03 +0530

    Removed View Source

--- src/components/NavigationFrame.js
+++ src/components/NavigationFrame.js
@@ -454,17 +454,7 @@ function Footer() {
   const classes = useFooterStyles();
   return (
     <footer className={classes.footer}>
-      <Button
-        variant="outlined"
-        color="primary"
-        component="a"
-        target="_blank"
-        rel="noopener"
-        href="https://github.com/serum-foundation/spl-token-wallet"
-        startIcon={<CodeIcon />}
-      >
-        View Source
-      </Button>
+      
     </footer>
   );
 }
