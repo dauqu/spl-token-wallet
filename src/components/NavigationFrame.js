07d0a03ce951319a421d942569716c4bea04eaa6
Harsh Singh <sujeetsingh734321@gmail.com>
2022-05-08 16:45:55 +0530

    Changed Branding

--- src/components/NavigationFrame.js
+++ src/components/NavigationFrame.js
@@ -95,7 +95,7 @@ export default function NavigationFrame({ children }) {
         )}
         <Toolbar>
           <Typography variant="h6" className={classes.title} component="h1">
-            {isExtensionWidth ? 'Sollet' : 'Solana SPL Token Wallet'}
+            {isExtensionWidth ? 'Sollet' : 'Cocucoin'}
           </Typography>
           <NavigationButtons />
         </Toolbar>
